3f1996d2014dd083d7862ff30b83bf8119ec94c3
JAlcocerT <jalcocert@fossengineer.com>
2024-06-19 21:40:32 +0200

    updating token

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ https://www.youtube.com/@DevTechBytes
 class Config:
     PAGE_TITLE = "Streamlit Ollama Chatbot"
 
-    OLLAMA_MODELS = ('codellama:7b', 'codellama:13b', 'llama2-uncensored', 
+    OLLAMA_MODELS = ('llama3:8b','qwen:0.5b','codellama:7b', 'codellama:13b', 'llama2-uncensored', 
                     'llama2:7b', 'llama2:13b', 'mistral', 'mixtral')
 
     SYSTEM_PROMPT = f"""You are a helpful chatbot that has access to the following 
